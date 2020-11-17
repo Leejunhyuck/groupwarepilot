42b69ceaa196cbe93b50c42b757a4952f763abb5
Lee <hg00521@naver.com>
2020-11-17 15:11:56 +0900

    redis-server

--- chat/project/src/main/java/org/raccoon/project/chat/controller/ChatController.java
+++ chat/project/src/main/java/org/raccoon/project/chat/controller/ChatController.java
@@ -1,25 +1,32 @@
 package org.raccoon.project.chat.controller;
 
 import org.raccoon.project.chat.model.ChatMessage;
+import org.raccoon.project.chat.pubsub.RedisPublisher;
+import org.raccoon.project.chat.repository.ChatRoomRepository;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
+
 
 
 
 @Controller
 public class ChatController {
-    private final SimpMessageSendingOperations messagingTemplate;
+    private final RedisPublisher redisPublisher;
+    private final ChatRoomRepository chatRoomRepository;
 
-    public ChatController(SimpMessageSendingOperations messagingTemplate){
-        this.messagingTemplate = messagingTemplate;
+    public ChatController(RedisPublisher redisPublisher, ChatRoomRepository chatRoomRepository){
+        this.redisPublisher = redisPublisher;
+        this.chatRoomRepository = chatRoomRepository;
     }
 
     @MessageMapping("/chat/message")
     public void message(ChatMessage message) {
-        if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            chatRoomRepository.enterChatRoom(message.getRoomId());
             message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        }
+        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
     }
     
 
