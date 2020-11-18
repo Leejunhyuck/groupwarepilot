34436f550623f55ffe8c3547c067d2aab8906931
Lee <hg00521@naver.com>
2020-11-18 11:05:51 +0900

    different import modified

--- chat/project/src/main/java/org/raccoon/project/chat/controller/ChatRoomController.java
+++ chat/project/src/main/java/org/raccoon/project/chat/controller/ChatRoomController.java
@@ -6,6 +6,8 @@ import org.raccoon.project.chat.model.ChatRoom;
 import org.raccoon.project.chat.model.LoginInfo;
 import org.raccoon.project.chat.repository.ChatRoomRepository;
 import org.raccoon.project.chat.service.JwtTokenProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+
 
 @Controller
 @RequestMapping("/chat")
